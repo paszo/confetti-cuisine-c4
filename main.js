fa085c9234a211a45185ae16ed8be7ce27ded397
paszo <paszo2008@gmail.com>
2020-07-24 18:11:45 +0200

    Added CRUD to subscriber

--- main.js
+++ main.js
@@ -6,6 +6,15 @@ const layouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
+const router = express.Router();
+router.get("/subscribers", subscribersController.index, subscribersController.indexView);
+router.get("/subscribers/new", subscribersController.new);
+router.post("/subscribers/create", subscribersController.create, subscribersController.redirectView);
+router.get("/subscribers/:id", subscribersController.show, subscribersController.showView);
+router.get("/subscribers/:id/edit", subscribersController.edit);
+router.put("/subscribers/:id/update", subscribersController.update, subscribersController.redirectView);
+router.delete("/subscribers/:id/delete", subscribersController.delete, subscribersController.redirectView);
+
 mongoose.Promise = global.Promise;
 
 const dbstring = process.env.CONFETTIDB;
@@ -22,13 +31,13 @@ app.use(express.urlencoded( {extended: false}));
 app.use(express.json());
 app.use(layouts);
 app.use(methodOverride("_method", {methods: ["POST", "GET"]}));
+app.use("/", router);
 
 app.get("/", homeController.showHome);
 app.get("/courses", homeController.showCourses);
-app.get("/contact", subscribersController.getSubscriptionPage);
+// app.get("/contact", subscribersController.getSubscriptionPage);
 // app.post("/contact", homeController.postedSignUpForm);
-app.get("/subscribers", subscribersController.getAllSubscribers);
-app.post("/subscribe", subscribersController.saveSubscriber);
+
 
 app.use(errorController.pageNotFoundError);
 app.use(errorController.internalServerError);
